3e1b2856665eab6b3ab1e8310f6fc9b605792eb1
Your Name <you@example.com>
2016-06-08 13:15:10 +0900

    my first commit

---
+++ omikuji_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  omikuji_app
+//
+//  Created by しょうじこうへい on 2016/06/08.
+//  Copyright © 2016年 KouheiShoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
