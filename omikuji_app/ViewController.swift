e6c46adeabaf68f493b4dd1e4f61db182159884d
Your Name <you@example.com>
2016-06-12 01:09:38 +0900

    おみくじ実装 next → tweet ボタン実装

--- omikuji_app/ViewController.swift
+++ omikuji_app/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var my_label: UILabel!
+    @IBAction func my_button(sender: AnyObject) {
+        let num = arc4random() % 5
+        print(num)
+        
+        switch num {
+        case 0:
+            my_label.text = "大吉"
+            print("0大吉")
+            break
+        case 1:
+            my_label.text = "吉"
+            print("吉")
+            break
+        case 2:
+            my_label.text = "小吉"
+            print("小吉")
+            break
+        case 3:
+            my_label.text = "中吉"
+            print("中吉")
+            break
+        case 4:
+            my_label.text = "凶"
+            print("凶")
+            break
+        default:
+            print("default")
+            break
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
