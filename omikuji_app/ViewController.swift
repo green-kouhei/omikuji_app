dcbdc689c9ea4061b6c27d9299c6aec6ce8c4645
Your Name <you@example.com>
2016-06-15 17:35:40 +0900

    ver 1.1	結果をtweet機能の追加 next	ver	SEの追加

--- omikuji_app/ViewController.swift
+++ omikuji_app/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
+import Social
+
 
 
 class ViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    var today_luck = "エラー"
+    
     //シェイクモーション
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
         if motion == UIEventSubtype.MotionShake {
@@ -18,13 +25,36 @@ class ViewController: UIViewController {
             
         }
     }
-    //メインラベル
+    //サウンドボタン
+    @IBOutlet weak var sound_button: UIButton!
+    
+    //ここで再生メゾット呼び出し
     @IBOutlet weak var my_label: UILabel!
+    
     //ボタン＆switch文
     @IBAction func my_button(sender: AnyObject) {
         a()
     }
     
+    //結果をtweet
+    @IBAction func tweet_button(sender: AnyObject) {
+        result_tweet()
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //おみくじ
     func a()  {
         let num = arc4random() % 8
         print(num)
@@ -51,21 +81,37 @@ class ViewController: UIViewController {
             print("凶")
             break
         default:
-            my_label.text = "エラー"
             print("エラー",num)
             break
             
         }
+        //結果をtweet用変数に設定
+        today_luck=my_label.text!
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //tweet
+    func result_tweet(){
+        // availability check
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            // snsの種類を定義
+            let controller = SLComposeViewController(forServiceType: SLServiceTypeTwitter)//SNSの種類
+            
+            // リンク追加
+            //let link: String = "http://www.apple.com"
+            //let url = NSURL(string: link)
+            //controller.addURL(url)
+            
+            // テキスト追加
+            let title: String = ("今日の運勢は\(today_luck)")
+            controller.setInitialText(title)
+            
+            // tweet画面を表示
+            presentViewController(controller, animated: true, completion: {})
+        }else{
+            print("エラー")
+        }
+        
     }
 }
